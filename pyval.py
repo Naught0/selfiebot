994118eda35eb205f63424b37e03a33c1455291c
James <naughtsee@gmail.com>
2017-09-26 18:31:10 -0400

    remove reaction

--- pyval.py
+++ pyval.py
@@ -64,10 +64,6 @@ class Admin:
             await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
         else:
             value = stdout.getvalue()
-            try:
-                await ctx.message.add_reaction('\u2705')
-            except:
-                pass
 
             if ret is None:
                 if value:
