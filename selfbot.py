2a574e0561d43c1ad016da004cea991628446d4e
James <naughtsee@gmail.com>
2017-09-26 17:47:43 -0400

    whoops

--- selfbot.py
+++ selfbot.py
@@ -1,10 +1,24 @@
 import discord
+import asyncpg
+import aiohttp
+from datetime import datetime
 from discord.ext import commands
+
 
 bot = commands.Bot(command_prefix="?>", self_bot=True)
 
-@bot.command()
-async def add(ctx, left: int, right: int):
-	await ctx.send(left + right)
+# For internet fun
+bot.aio_session = aiohttp.ClientSession
+
+# Create connection to postgresql server using pools
+async def create_db_pool():
+    with open('data/apikeys.json') as f:
+        pg_pw = json.load(f)['postgres']
+    bot.pg_con = await asyncpg.create_pool(user='james', password=pg_pw, database='discord_testing')
+bot.loop.run_until_complete(create_db_pool())
+
+@bot.event
+async def on_ready():
+	print(f'Client logged in at {datetime.now()}')
 
 bot.run("mfa.Kd6-eyiOy4gD8cXDpT1m-1t7wE69Vx-cavTm3KX5yKZSfFlP_6txvZaE6ql4Nuw3xQnGaR3m_uP6_S4oh5Y2", bot=False)
