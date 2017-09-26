83ecd90d981497134d957d5219e53c5bbb24b8b1
James <naughtsee@gmail.com>
2017-09-26 18:22:24 -0400

    just copypaste the whole thing

--- eval.py
+++ eval.py
@@ -14,13 +14,44 @@ class Eval:
         stdout, stderr = await process.communicate()
         try:
             if stdout:
-                await ctx.send(f'`{cmd}`\n```{stdout.decode().strip()}```\n```{stderr.decode().strip()}')
+                await ctx.send(f'`{cmd}`\n```{stdout.decode().strip()}```')
+            elif stderr:
+                await ctx.send(f'`{cmd}`\n```{stderr.decode().strip()}```')
             else:
                 await ctx.send(f'`{cmd}` produced no output')
 
         except Exception as e:
             await ctx.send(f'Unable to send output\n```py\n{e}```')
 
+    @commands.group(invoke_without_command=True, name='sql', hidden=True)
+    async def sql_execute(self, ctx, *, query):
+        """ Lets me access the postgres database via discord """
+        try:
+            res = await self.db_conn.execute(query)
+        except Exception as e:
+            return await ctx.send(f'```py\n{type(e).__name__}\n{str(e)}```')
+
+        if not res:
+            return await ctx.send(f'Sorry, `{query}` did not return anything.')
+
+        await ctx.send(f'```sql\n{res}```')
+
+    @sql_execute.command(name='fetch')
+    async def sql_fetch(self, ctx, *, query):
+        try:
+            res = await self.db_conn.fetch(query)
+        except Exception as e:
+            return await ctx.send(f'```py\n{type(e).__name__}\n{str(e)}```')
+
+        if not res:
+            return await ctx.send(f'Sorry, `{query}` did not return anything.')
+
+        em = discord.Embed(color=discord.Color.dark_orange(), title='SQL Fetch')
+        for k, v in res[0].items():
+            em.add_field(name=k, value=v)
+
+        await ctx.send(embed=em)
+
+
 def setup(bot):
     bot.add_cog(Eval(bot))
-    
