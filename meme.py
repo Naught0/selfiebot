cc2009d742bd06cc96bd91651412eefd30a6251d
James <naughtsee@gmail.com>
2017-09-30 03:37:53 -0400

    add aesthetic

---
+++ meme.py
@@ -0,0 +1,20 @@
+#!/bin/env python3
+
+from discord.ext import commands
+
+
+class Aesthetic:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='at')
+    async def aesthetify(self, ctx, bot):
+        """ Aesthetify some text """
+        ascii_to_wide = dict((i, chr(i + 0xfee0)) for i in range(0x21, 0x7f))
+        ascii_to_wide.update({0x20: u'\u3000', 0x2D: u'\u2212'})
+
+        await ctx.message.edit(ctx.message, f'a_text.translate(ascii_to_wide)}')
+
+
+def setup(bot):
+    bot.add_cog(Aesthetic(bot))
